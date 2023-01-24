7ab02151e8ff3538ffda5acb477f4754f6254313
Vicky Ricart <vickyc.ricart@gmail.com>
2023-01-24 14:42:33 +0100

    Html Game Board, Basic Css Setup & Begin JS

---
+++ script.js
@@ -0,0 +1,62 @@
+ const statusDisplay = document.querySelector('.game-status');
+
+let gameActive = true;
+
+let currentPlayer = 'X'
+
+let gameState = ['', '', '', '', '', '', '', '', ''];
+
+const winningConditions = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `It's a Tie!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+
+}
+
+function handleResultValidation() {
+
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+
+    const clickedCellIndex = parseInt(
+        clickedCell.getAttribute('data-cell-index')
+    );
+
+    if(gameState[clickedCellIndex] !== '' || !gameActive) {
+        return;
+    } 
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame () {
+
+}
+
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game-restart').addEventListener('click', handleRestartGame);
+
